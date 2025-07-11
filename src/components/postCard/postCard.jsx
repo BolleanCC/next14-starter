75935ac6013afa6aa8dfcb51bf15089a9ec527a6
BolleanCC <songtao19980820@outlook.com>
2025-07-11 00:33:57 -0600

    Added post card design

---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,35 @@
+import styles from "./postCard.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg"
+            alt=""
+            fill
+            className={styles.img}
+          />
+        </div>
+        <span className={styles.date}>01.01.2024</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi,
+          suscipit est illum nesciunt, optio excepturi officia nulla dolor
+          recusandae dignissimos hic tenetur sunt, cumque esse placeat ipsum
+          quam inventore error.
+        </p>
+        <Link className={styles.link} href="/blog/post">
+          READ MORE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
